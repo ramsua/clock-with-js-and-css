9d14802bf7e8ada537102080afdb45ee9d9f70fe
ramsua <richardramsua@gmail.com>
2020-08-04 01:14:50 -0300

    Return to tge method before recursion by performance issue

--- src/js/modules/clock.js
+++ src/js/modules/clock.js
@@ -11,7 +11,16 @@ const analogicClock = () => {
 
     setInterval(() => {
 
-        analogicClock()
+        // Se iba a usar recursividad
+        // pero tiene un límite de llamados así que
+        // Dentro de lo que sé esto es lo que pude hacer.
+        // Per SÉ también, que no se debe repetir código
+
+        const time = date()
+
+        rootVars.setProperty('--ht-hours', `${time.hours * 30}deg`)
+        rootVars.setProperty('--ht-minutes', `${time.minutes * 6}deg`)
+        rootVars.setProperty('--ht-seconds', `${time.seconds * 6}deg`)
 
     }, 1000);
 }
